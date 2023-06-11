c85efb9294a02ece912c17c074af793d643b3ae8
Gaurav12342 <gauravpendharkar2@gmail.com>
2023-06-11 18:36:32 +0530

    Added web socket changes

---
+++ src/pages/Home.tsx
@@ -0,0 +1,34 @@
+import { FC, useEffect, useState } from "react";
+import { connect } from "socket.io-client";
+const Home: FC = () => {
+  const socket = connect("http://localhost:4000");
+  const [userMessage, setUserMessage] = useState("");
+  const [getResponse, setResponse] = useState<any>("");
+  console.log("🚀 ~ file: Home.tsx:7 ~ getResponse:", getResponse);
+
+  useEffect(() => {
+    socket.on("receive_message", (args) => {
+      setResponse(args);
+    });
+  }, [userMessage]);
+
+  const handleSubmit = () => {
+    socket.emit("send_message", { message: userMessage });
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        name="message"
+        onChange={(e: any) => setUserMessage(e.target.value)}
+      />
+      <button type="submit" onClick={handleSubmit}>
+        Submit
+      </button>
+
+      <h1>{getResponse?.message}</h1>
+    </div>
+  );
+};
+
+export default Home;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,8 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import { routesConstant } from "./constant";
 import RecipesList from "../pages/RecipesList";
 import RecipeById from "../pages/RecipeById";
+import Home from "../pages/Home";
 
 const index: any = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
   {
     path: routesConstant.recipe.path,
     element: <RecipesList />,
